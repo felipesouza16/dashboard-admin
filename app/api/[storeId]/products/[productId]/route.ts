ee368ac655e80295c0533adbb62c6d2235bb5ee8
Felipe Souza <fesouza2345@gmail.com>
2023-09-26 19:30:20 -0300

    feat: created api routes

---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,171 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse("O id do produto é obrigatório", { status: 400 });
+    }
+
+    const product = await prismadb.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        category: true,
+        images: true,
+        color: true,
+        size: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_GET]", error);
+    return new NextResponse("Erro interno.", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const {
+      name,
+      price,
+      categoryId,
+      colorId,
+      sizeId,
+      images,
+      isFeatured,
+      isArchived,
+    } = body;
+
+    if (!userId) {
+      return new NextResponse("Não autenticado.", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("O nome é obrigatório.", { status: 400 });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse("As imagens são obrigatórias.", { status: 400 });
+    }
+
+    if (!price) {
+      return new NextResponse("O preço é obrigatório.", { status: 400 });
+    }
+
+    if (!categoryId) {
+      return new NextResponse("A categoria é obrigatória.", { status: 400 });
+    }
+
+    if (!colorId) {
+      return new NextResponse("A cor é obrigatória.", { status: 400 });
+    }
+
+    if (!sizeId) {
+      return new NextResponse("O tamanho é obrigatório.", { status: 400 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("O id do produto é obrigatório", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Não autorizado.", { status: 403 });
+    }
+
+    await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price,
+        isFeatured,
+        isArchived,
+        categoryId,
+        colorId,
+        sizeId,
+        images: {
+          deleteMany: {},
+        },
+      },
+    });
+
+    const product = await prismadb.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_PATCH]", error);
+    return new NextResponse("Erro interno.", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string; productId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Não autenticado.", { status: 401 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse("O id do produto é obrigatório", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Não autorizado.", { status: 403 });
+    }
+
+    const product = await prismadb.product.deleteMany({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log("[PRODUCT_DELETE]", error);
+    return new NextResponse("Erro interno.", { status: 500 });
+  }
+}
