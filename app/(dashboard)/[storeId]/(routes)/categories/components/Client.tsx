021e4bcf154ffb9db5201dbf74a84c6e9387f55e
Felipe Souza <fesouza2345@gmail.com>
2023-09-26 19:28:06 -0300

    feat: created page for categories

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/Client.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { CategoryColumn, columns } from "./Columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+type CategoryClientProps = {
+  data: CategoryColumn[];
+};
+
+export const CategoryClient = ({ data }: CategoryClientProps) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Categorias (${data.length})`}
+          description="Gerencie as categorias da sua loja."
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Adicionar
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API para chamar as categorias" />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
