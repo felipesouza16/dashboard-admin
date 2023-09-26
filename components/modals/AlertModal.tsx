9494aed03eada0852d927ae28623b19a9a5f95c6
Felipe Souza <fesouza2345@gmail.com>
2023-09-26 19:27:16 -0300

    feat: created modal alert component

---
+++ components/modals/AlertModal.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import { Modal } from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+
+type AlertModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+};
+
+export const AlertModal = ({
+  isOpen,
+  loading,
+  onClose,
+  onConfirm,
+}: AlertModalProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <Modal
+      title="Você tem certeza?"
+      description="Esse ato não pode ser desfeito."
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant="outline" onClick={onClose}>
+          Cancelar
+        </Button>
+        <Button disabled={loading} variant="destructive" onClick={onConfirm}>
+          Continuar
+        </Button>
+      </div>
+    </Modal>
+  );
+};
